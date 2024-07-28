355029b9f853742d88302927d27377a17ff0073d
NikitoshSykes <nikitoshtor@gmail.com>
2024-07-28 14:15:07 +0300

    Added a function to the program that outputs the nth Fibonacci number
    The function recursively calculates the nth number.
    If n is 1 or 2, then the Fibonacci number is 1.
    Otherwise, the return of the sum of the previous values of the function

--- prog1.cpp
+++ prog1.cpp
@@ -1,6 +1,19 @@
 #include <iostream> 
 
+int fib(int n){
+	if(n == 1 || n == 2){
+		return 1;
+	}
+	else{
+		return fib(n-1) + fib(n-2);
+	}
+}
+
 int main(){
+	int n;
 	std::cout << "Hello, world!" <<  std::endl;
+	std::cout << "Enter n: "; 
+	std::cin >> n;
+	std::cout << n << "-ое число Фиббоначи - это " << fib(n) << std::endl;
 	return 0;
 }
