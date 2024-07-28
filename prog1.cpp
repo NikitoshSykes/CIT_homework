6ed7d4768b80fa8128e8dc6230aa1c37c4bf6437
NikitoshSykes <nikitoshtor@gmail.com>
2024-07-28 14:59:13 +0300

    Changed the function in the program for the second time, which outputs the nth Fibonacci number
    Now the function iteratively calculates the nth number.
    if n is 1, output 0 if n = 2, output 1.
    Otherwise, we add the previous one to the last Fibonacci number in a cycle, obviously saving the value and passing it on to the first number.

--- prog1.cpp
+++ prog1.cpp
@@ -1,14 +1,26 @@
 #include <iostream> 
 
 int fib(int n){
+	int num1 = 0;
+	int num2 = 1;
 	if(n == 1){
-		return 0;
+		return num1;
 	}
 	else if(n == 2){
-		return 1;
+		std::cout << num1 << std::endl;
+		return num2;
 	}
 	else{
-		return fib(n-1) + fib(n-2);
+		std::cout << num1 << std::endl << num2 << std::endl;
+		for(int i = 3; i <= n; i++){
+			if(i != 3){
+				std::cout << num1 << std::endl;
+			}
+			int safeInt = num2;
+			num2 += num1;
+			num1 = safeInt;
+		}
+		return num2;
 	}
 }
 
@@ -17,6 +29,6 @@ int main(){
 	std::cout << "Hello, world!" <<  std::endl;
 	std::cout << "Enter n: "; 
 	std::cin >> n;
-	std::cout << n << "-ое число Фиббоначи - это " << fib(n) << std::endl;
+	std::cout << fib(n) << std::endl;
 	return 0;
 }
